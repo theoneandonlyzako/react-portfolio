6e6873fe1f30619ed0e0e121d2206cd09eb86b5b
Zak Stamps <theoneandonlyzako@gmail.com>
2021-08-06 17:43:15 -0700

    Updates

---
+++ precache-manifest.d74757d4a45446bcfd04e505070127fa.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5dd54b6ec8ebfe5929bf7d1375d2ed1",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "758356a770cd58e1c198",
+    "url": "/react-portfolio/static/css/2.9ad4a810.chunk.css"
+  },
+  {
+    "revision": "334594df9442565aa3f1",
+    "url": "/react-portfolio/static/css/main.690580d0.chunk.css"
+  },
+  {
+    "revision": "758356a770cd58e1c198",
+    "url": "/react-portfolio/static/js/2.ce73187f.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/react-portfolio/static/js/2.ce73187f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "334594df9442565aa3f1",
+    "url": "/react-portfolio/static/js/main.57009158.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "ff2b7924b27bae7160c8881549e50397",
+    "url": "/react-portfolio/static/media/blog.ff2b7924.png"
+  },
+  {
+    "revision": "ea2539891e5a04b9a850012fe8fc659b",
+    "url": "/react-portfolio/static/media/budget_tracker.ea253989.png"
+  },
+  {
+    "revision": "66249afdc13ed3f933c5c6c87d12c81b",
+    "url": "/react-portfolio/static/media/generator.66249afd.png"
+  },
+  {
+    "revision": "cee191a40e22130a41298ad886ea42a1",
+    "url": "/react-portfolio/static/media/notes.cee191a4.png"
+  },
+  {
+    "revision": "34578ed596a3fb692f5c6bd468e84577",
+    "url": "/react-portfolio/static/media/profilepic.34578ed5.png"
+  },
+  {
+    "revision": "7cc372cdeaacb837f84527072edce08b",
+    "url": "/react-portfolio/static/media/pursuit.7cc372cd.png"
+  },
+  {
+    "revision": "4b577d931286d2d70d5bebed64bac47e",
+    "url": "/react-portfolio/static/media/weather_dashboard.4b577d93.png"
+  }
+]);
