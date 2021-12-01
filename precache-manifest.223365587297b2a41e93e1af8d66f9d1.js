35d1003eeda1cb63326e332e18493dfd34119b1d
Zak Stamps <77663546+theoneandonlyzako@users.noreply.github.com>
2021-11-30 17:40:21 -0800

    Updates

--- precache-manifest.223365587297b2a41e93e1af8d66f9d1.js
+++ precache-manifest.223365587297b2a41e93e1af8d66f9d1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a8afc954abdbc71d0ea1e4e1afc3e680",
+    "revision": "b188b6418f816431b71b34bcb9532de3",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.9ad4a810.chunk.css"
   },
   {
-    "revision": "e03da66e52474df14444",
+    "revision": "d95257c1e261117a2f45",
     "url": "/static/css/main.5de1b5cf.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.c2ed514e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e03da66e52474df14444",
-    "url": "/static/js/main.e461404c.chunk.js"
+    "revision": "d95257c1e261117a2f45",
+    "url": "/static/js/main.1108262f.chunk.js"
   },
   {
     "revision": "dd8147bbe7734801bdde",
