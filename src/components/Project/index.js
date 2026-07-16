8f25d8c9de41aba7ada70bb070819ceb16cec079
Zak Stamps <theoneandonlyzako@gmail.com>
2026-07-15 23:12:42 -0700

    Project card: render title/image without a link when a card has no deployed_app

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -4,13 +4,18 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 
 function Project({project}) {
+  const hasLink = Boolean(project.deployed_app);
+
   return (
     <Card className="d-flex bg-dark justify-content-center">
         <Card.Body>
           <Card.Title className="d-flex justify-content-between align-items-center">
-            <a href={project.deployed_app} target="_blank" rel="noopener noreferrer">
-              <h4 className="text-light">{project.title}</h4>
-            </a>
+            {hasLink
+              ? <a href={project.deployed_app} target="_blank" rel="noopener noreferrer">
+                  <h4 className="text-light">{project.title}</h4>
+                </a>
+              : <h4 className="text-light">{project.title}</h4>
+            }
             {project.repository &&
               <a href={project.repository} target="_blank" rel="noopener noreferrer">
                 <span><i className="fab fa-github"></i></span>
@@ -20,9 +25,12 @@ function Project({project}) {
           <Card.Subtitle className="text-light">
             {project.description}
           </Card.Subtitle>
-        <a href={project.deployed_app} target="_blank" rel="noopener noreferrer">
-          <Card.Img src={project.imageSrc} className="pt-4"/>
-        </a>
+          {hasLink
+            ? <a href={project.deployed_app} target="_blank" rel="noopener noreferrer">
+                <Card.Img src={project.imageSrc} className="pt-4"/>
+              </a>
+            : <Card.Img src={project.imageSrc} className="pt-4"/>
+          }
         </Card.Body>
     </Card>
   )
